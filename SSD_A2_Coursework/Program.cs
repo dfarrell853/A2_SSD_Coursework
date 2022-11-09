faeaffc8f64a336b50a067cde784cb8305429983
dfarr <dfarr@LAPTOP-PU3UKF7H>
2022-11-09 14:23:01 +0000

    Add project files.

---
+++ SSD_A2_Coursework/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSD_A2_Coursework
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        private static void SetupDataDictionaryPath()
+        {
+            string debugPath = System.IO.Path.GetDirectoryName(Environment.CurrentDirectory);
+            string dataDirectoryPath = System.IO.Path.GetDirectoryName(debugPath);
+            AppDomain.CurrentDomain.SetData("DataDirectory", dataDirectoryPath);
+        }
+    }
+}
